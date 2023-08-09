56eef6ac9878cad373d6624545a2b41e979f8990
Steffano da Cruz <steffanodacruz@outlook.com>
2023-08-09 13:15:39 -0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -3,74 +3,90 @@
 let secretNumber = Math.trunc(Math.random() * 20 + 1);
 let scoreValue = 20;
 let highscoreValue = 0;
-const checkButton = document.querySelector(".check");
-const message = document.querySelector(".message");
-const number = document.querySelector(".number");
-const bodyColor = document.querySelector("body");
+
+//Selecting elements
 const score = document.querySelector(".score");
 const highscore = document.querySelector(".highscore");
+const checkButton = document.querySelector(".check");
 const againButton = document.querySelector(".again");
+const message = document.querySelector(".message");
+const number = document.querySelector(".number");
+const backColor = document.querySelectorAll(".back-color");
+const inputFocus = document.querySelector(".guess");
+const guess = document.querySelector(".guess");
 
-const displayMessage = function (resp) {
-  message.textContent = resp;
-};
-const displayScore = function (resp) {
-  score.textContent = resp;
-};
-
-const displayNumber = function (resp) {
-  number.textContent = resp;
+const displayMessage = function (result) {
+  message.textContent = result;
 };
 
-const displayColor = function (resp) {
-  bodyColor.style.backgroundColor = resp;
+const displayColor = function (result) {
+  for (let i = 0; i < backColor.length; i++) {
+    backColor[i].style.backgroundColor = result;
+  }
 };
 
-const displayWidth = function (resp) {
-  number.style.width = resp;
+const displayScore = function (result) {
+  score.textContent = result;
 };
+
+const displayWidth = function (result) {
+  number.style.width = result;
+};
+
+const displayNumber = function (result) {
+  number.textContent = result;
+};
+
+const displayGuess = function (result) {
+  guess.value = result;
+};
+
+inputFocus.focus();
 
 checkButton.addEventListener("click", function () {
-  const guess = Number(document.querySelector(".guess").value);
+  const guessValue = Number(guess.value);
 
   //When there's no input
-  if (guess === 0 || !guess) {
+  if (guessValue === 0 || !guessValue) {
     displayMessage("⛔ No number...");
 
     //When the player wins
-  } else if (guess === secretNumber) {
+  } else if (guessValue === secretNumber) {
     displayMessage("🎉 Correct number!");
-    displayNumber(secretNumber);
     displayColor("#60b347");
-    displayWidth("8rem");
+    displayWidth("10rem");
+    displayNumber(secretNumber);
+
     if (scoreValue > highscoreValue) {
       highscoreValue = scoreValue;
       highscore.textContent = highscoreValue;
     }
 
-    //When the player chooses the wrong number
-  } else if (guess > secretNumber || guess < secretNumber) {
-    guess > secretNumber
+    //When the guess is wrong
+  } else if (guessValue > secretNumber || guessValue < secretNumber) {
+    guessValue > secretNumber
       ? displayMessage("📈 Too high!")
       : displayMessage("📉 Too low!");
     scoreValue--;
     displayScore(scoreValue);
     if (scoreValue < 1) {
-      displayMessage("💥 You lost the game");
-      displayScore("0");
+      displayMessage("💥 You lost the game...");
+      displayScore(0);
     }
   }
 });
 
-//When the player wants to start over
+//When the player starts over
 againButton.addEventListener("click", function () {
+  inputFocus.focus();
+
   secretNumber = Math.trunc(Math.random() * 20 + 1);
   scoreValue = 20;
 
   displayNumber("?");
   displayScore(scoreValue);
-  displayColor("#222");
-  displayWidth("5rem");
   displayMessage("Start guessing...");
-  document.querySelector(".guess").value = "";
+  displayColor("#222");
+  displayWidth("5.2rem");
+  displayGuess("");
 });
